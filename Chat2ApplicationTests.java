a9520029cb3f1f0d9cfc390b4b63a295ec379b07
oliveirablenda <121172966+oliveirablenda@users.noreply.github.com>
2023-08-28 18:10:14 -0400

    Add files via upload

---
+++ Chat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifam.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
